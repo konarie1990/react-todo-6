c52d3bac19bef02dc297998b6008f4148f2a67e6
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-07 14:28:48 -0600

    working on delete todo

--- src/List.js
+++ src/List.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./List.css";
 
-const List = props => (
-  <ul>
-    {props.items.map((item, index) => (
-      <li key={index}>{item}</li>
-    ))}
-  </ul>
-);
+class List extends Component {
+  delete = key => this.state.delete(key);
+
+  List = props => {
+    return (
+      <ul>
+        {props.items.map((item, index) => (
+          <li onClick={() => this.delete(item.key)} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  render() {
+    const todoEntries = this.props.entries;
+    const listItems = todoEntries.map(this.createTasks);
+    return <ul className="theList">{listItems}</ul>;
+  }
+}
 
 export default List;
